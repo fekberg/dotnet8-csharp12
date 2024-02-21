61322be1e66f2e7ce6bf84704ef1fe633f8c4006
Filip Ekberg <filip@ekberg.dev>
2024-02-21 20:27:18 +0100

    Remove RC

--- Demo 6/Interceptor.cs
+++ Demo 6/Interceptor.cs
@@ -7,7 +7,7 @@ public static class Interceptor
 {
     [DebuggerHidden]
     [InterceptsLocation
-        (@"Y:\Code\dotnet8-csharp12\Demo 6\Logger.cs", 7, 9)]
+        (@"C:\Code\dotnet8-csharp12\Demo 6\Logger.cs", 7, 9)]
     public static void DebugLog(this Logger logger, string message)
     {
         Console.WriteLine(message);
